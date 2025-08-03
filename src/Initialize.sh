39b8d7c1b6732c73fabe26064c619298ca06a6d3
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-08-02 22:15:46 -0400

    adding intializing script for preparing environment for output files

---
+++ src/Initialize.sh
@@ -0,0 +1,5 @@
+# If res directory doesn't exist, then create it.
+if [ ! -d ../res ]; then
+    mkdir ../res
+    echo Created ../res directory 
+fi
---
+++ testing/Initialize.sh
@@ -0,0 +1,5 @@
+# If bin directory doesn't exist, then create it.
+if [ ! -d ../bin ]; then
+    mkdir ../bin
+    echo Created ../bin directory 
+fi 
