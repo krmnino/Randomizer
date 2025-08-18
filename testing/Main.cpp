adbc7cc1b3e2096dab9c2c161b3015ed3f482232
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-08-17 23:00:58 -0400

    added example code for shuffle method

--- testing/Main.cpp
+++ testing/Main.cpp
@@ -1,11 +1,22 @@
 #include "../src/Randomizer.hpp"
-#include <iostream>
+
+#define N_ELEMENTS 20
+#define BIG_OBJ_SIZE 0x1000
+#define BYTES_PER_LINE 64
 
 Randomizer* Randomizer::rnd_ptr = nullptr;
 
 int main(){
     std::string alphanum_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     std::string empty_str = "";
+    struct BigObject{
+        uint8_t data[BIG_OBJ_SIZE];
+    };
+    typedef struct BigObject BigObject;
+    uint8_t* array_u8;
+    uint32_t* array_u32;
+    uint64_t* array_u64;
+    BigObject* array_bigobj;
 
     Randomizer* rnd = Randomizer::get_instance(1);
     if(rnd == nullptr){
@@ -85,7 +96,121 @@ int main(){
         printf("Pseudorandom bool #%ld -> %s\n", i + 1, rnd->gen_bool() ? "true" : "false");
     }
     rnd->root_seed_next();
+    
+    // Shuffling array of uint8_t
+    array_u8 = new uint8_t[20];
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        array_u8[i] = i + 1;
+    }
+    printf("List of uint8_t [BEFORE]: [");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("%d", array_u8[i]);
+        if(i < N_ELEMENTS -1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    rnd->shuffle(array_u8, N_ELEMENTS, sizeof(uint8_t));
+    printf("List of uint8_t [AFTER]:  [");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("%d", array_u8[i]);
+        if(i < N_ELEMENTS - 1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    delete[] array_u8;
+    rnd->root_seed_next();
+    
+    // Shuffling array of uint32_t
+    array_u32 = new uint32_t[20];
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        array_u32[i] = (i + 1) * 2;
+    }
+    printf("List of uint32_t [BEFORE]: [");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("%d", array_u32[i]);
+        if(i < N_ELEMENTS -1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    rnd->shuffle(array_u32, N_ELEMENTS, sizeof(uint32_t));
+    printf("List of uint32_t [AFTER]:  [");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("%d", array_u32[i]);
+        if(i < N_ELEMENTS - 1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    delete[] array_u32;
+    rnd->root_seed_next();
 
+    // Shuffling array of uint64_t
+    array_u64 = new uint64_t[20];
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        array_u64[i] = (i + 1) * 2;
+    }
+    printf("List of uint64_t [BEFORE]: [");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("%ld", array_u64[i]);
+        if(i < N_ELEMENTS -1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    rnd->shuffle(array_u64, N_ELEMENTS, sizeof(uint64_t));
+    printf("List of uint64_t [AFTER]:  [");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("%ld", array_u64[i]);
+        if(i < N_ELEMENTS - 1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    delete[] array_u64;
+    rnd->root_seed_next();
+    
+    // Shuffling array of BigObjs
+    array_bigobj = new BigObject[N_ELEMENTS];
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        for(size_t j = 0; j < BIG_OBJ_SIZE; j++){
+            array_bigobj[i].data[j] = (uint8_t)i;
+        }
+    }
+    printf("List of BigObj [BEFORE]:\n");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("\n\nElement# %ld\n", i);
+        for(size_t j = 0; j < BIG_OBJ_SIZE; j++){
+            if(j != 0 && j % 64 == 0){
+                printf("\n");
+            }
+            if(array_bigobj[i].data[j] < 0xa){
+                printf("0");
+            }
+            printf("%d", array_bigobj[i].data[j]);
+        }
+    }
+    printf("\n");
+    rnd->shuffle(array_bigobj, N_ELEMENTS, sizeof(BigObject));
+    printf("List of BigObj [AFTER]:\n");
+    for(size_t i = 0; i < N_ELEMENTS; i++){
+        printf("\n\nElement# %ld\n", i);
+        for(size_t j = 0; j < BIG_OBJ_SIZE; j++){
+            if(j != 0 && j % 64 == 0){
+                printf("\n");
+            }
+            if(array_bigobj[i].data[j] < 0xa){
+                printf("0");
+            }
+            printf("%d", array_bigobj[i].data[j]);
+        }
+    }
+    printf("\n");
+
+    delete[] array_bigobj;
+    
     Randomizer::end_instance();
     return 0;
 }
