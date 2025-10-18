092ab2b5a68697555e3f6359cd186618dfd289e1
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-10-17 21:44:55 -0400

    started C interface header file

---
+++ src/Randomizer_C.h
@@ -0,0 +1,16 @@
+#ifndef RANDOMIZER_C
+#define RANDOMIZER_C
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct Rnadomizer_C Rnadomizer_C;
+
+Rnadomizer_C* Rnadomizer_C_get_instance();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ifndef RANDOMIZER_C
