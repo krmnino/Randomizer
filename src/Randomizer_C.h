cee15f06c916828808dbfdc74bf83aa99782eee7
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-10-20 23:56:23 -0400

    defined Randomizer_C_gen_integral() and Randomizer_C_gen_integral_range() functions for the C interface

--- src/Randomizer_C.h
+++ src/Randomizer_C.h
@@ -1,15 +1,21 @@
 #ifndef RANDOMIZER_C
 #define RANDOMIZER_C
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
 #ifdef __cplusplus
 extern "C"{
 #endif
 
 typedef struct Randomizer_C Randomizer_C;
 
-Randomizer_C* Randomizer_C_get_instance();
+Randomizer_C* Randomizer_C_get_instance(uint32_t);
 void Randomizer_C_root_seed_next(Randomizer_C*);
 void Randomizer_C_root_seed_prev(Randomizer_C*);
+uint64_t Randomizer_C_gen_integral(Randomizer_C*);
+uint64_t Randomizer_C_gen_integral_range(Randomizer_C*, uint64_t, uint64_t);
 
 #ifdef __cplusplus
 }
