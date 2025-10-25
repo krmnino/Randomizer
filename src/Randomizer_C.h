aa16955496bcc1dc3bb268b3dcc1e5dd7813baf8
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-10-25 18:41:20 -0400

    implemented Randomizer_C_gen_integral_range() function for the C interface

--- src/Randomizer_C.h
+++ src/Randomizer_C.h
@@ -17,6 +17,7 @@ void Randomizer_C_root_seed_prev(Randomizer_C*);
 uint32_t Randomizer_C_get_root_seed(Randomizer_C*);
 uint32_t Randomizer_C_get_branching_seed(Randomizer_C*);
 bool Randomizer_C_gen_bool(Randomizer_C*);
+int Randomizer_C_gen_string(Randomizer_C*, char*, size_t, const char*);
 double Randomizer_C_gen_double_not_nan(Randomizer_C*);
 double Randomizer_C_gen_double_not_nan_range(Randomizer_C*, double, double);
 uint64_t Randomizer_C_gen_integral(Randomizer_C*);
