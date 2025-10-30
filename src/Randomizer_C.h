03686f6dd008a597047784b8cd1563563bb0192f
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2025-10-30 00:42:16 -0400

    implemented shuffle algorithm in header file for C interface

--- src/Randomizer_C.h
+++ src/Randomizer_C.h
@@ -23,6 +23,28 @@ double Randomizer_C_gen_double_not_nan_range(Randomizer_C*, double, double);
 uint64_t Randomizer_C_gen_integral(Randomizer_C*);
 uint64_t Randomizer_C_gen_integral_range(Randomizer_C*, uint64_t, uint64_t);
 
+int Randomizer_C_shuffle(Randomizer_C* rndc, void* array, size_t n_elements, size_t elem_size){
+    size_t rand_idx;
+    // Check for zeros or NULL
+    if(array == NULL || n_elements == 0 || elem_size == 0){
+        return -1;
+    }
+    // Nothing to shuffle
+    if(n_elements == 1){
+        return 0;
+    }
+    uint8_t* buff;
+    buff = (uint8_t*)calloc(1, elem_size);
+    // Variation of the Fisher-Yates shuffling algorithm
+    for(size_t i = 0; i < n_elements - 1; i++){
+        rand_idx = Randomizer_C_gen_integral_range(rndc, i, (n_elements - 1));
+        memcpy((uint8_t*)buff, (uint8_t*)array + (i * elem_size), elem_size);
+        memcpy((uint8_t*)array + (i * elem_size), (uint8_t*)array + (rand_idx * elem_size), elem_size);
+        memcpy((uint8_t*)array + (rand_idx * elem_size), (uint8_t*)buff, elem_size);
+    }
+    free(buff);
+    return 0;
+}
 #ifdef __cplusplus
 }
 #endif
