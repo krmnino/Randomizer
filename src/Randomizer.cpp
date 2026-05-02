453bb16030c69f590aea0d43fdf332e1a5cc98b6
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2026-05-02 00:45:07 -0400

    moved Randomizer_C_shuffle() definition to Randomizer.cpp file, miscellaneous fixes

--- src/Randomizer.cpp
+++ src/Randomizer.cpp
@@ -6,41 +6,50 @@ Randomizer::Randomizer(){
     this->branching_seed = this->c11_minstd(this->root_seed);
 }
 
+
 Randomizer::Randomizer(uint32_t input_seed){
     this->root_seed = input_seed;
     this->branching_seed = this->c11_minstd(this->root_seed);
 }
+
 
 uint32_t Randomizer::c11_minstd(uint32_t seed){
     int32_t ret = (48271 * (seed % 44488)) - (3399 * (seed / 44488));
     return ret < 0 ? ret + (0x80000000 - 1) : ret;
 }
 
+
 uint32_t Randomizer::c11_minstd_backwards(uint32_t seed){
     return -247665088 * seed % (0x80000000 - 1);
 }    
+
 
 void Randomizer::root_seed_next(){
     this->root_seed = c11_minstd(this->root_seed);
     this->branching_seed = c11_minstd(this->root_seed);
 }
 
+
 void Randomizer::root_seed_prev(){
     this->root_seed = c11_minstd_backwards(this->root_seed);
     this->branching_seed = c11_minstd_backwards(this->root_seed);
 }
 
+
 uint32_t Randomizer::get_root_seed(){
     return this->root_seed;
 }
+
 
 uint32_t Randomizer::get_branching_seed(){
     return this->branching_seed;
 }
 
+
 bool Randomizer::gen_bool(){
     return (this->gen_integral_range<uint8_t>(0, 1) == 0) ? false : true;
 }
+
 
 std::string Randomizer::gen_string(size_t out_str_length, const char* dictionary){
     const char* default_ascii_dict =  "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
@@ -54,9 +63,11 @@ std::string Randomizer::gen_string(size_t out_str_length, const char* dictionary
     return buffer.str();
 }
 
+
 std::string Randomizer::gen_string(size_t out_str_length, std::string& dictionary){
     return this->gen_string(out_str_length, dictionary.c_str());
 }
+
 
 double Randomizer::gen_double_not_nan(){
     union{
@@ -77,6 +88,7 @@ double Randomizer::gen_double_not_nan(){
     return buff.rand_buff_double;
 }
 
+
 double Randomizer::gen_double_not_nan_range(double lower, double upper){
     double ret;
     while (true){
@@ -96,6 +108,7 @@ Randomizer_C* Randomizer_C_init(uint32_t input_seed){
     return reinterpret_cast<Randomizer_C*>(new Randomizer(input_seed));
 }
 
+
 int Randomizer_C_delete(Randomizer_C* rndc){
     if(rndc == nullptr){
         return -1;
@@ -104,25 +117,31 @@ int Randomizer_C_delete(Randomizer_C* rndc){
     return 0;
 }
 
+
 void Randomizer_C_root_seed_next(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->root_seed_next();
 }
+
 
 void Randomizer_C_root_seed_prev(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->root_seed_prev();
 }
 
+
 uint32_t Randomizer_C_get_root_seed(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->get_root_seed();
 }
+
 
 uint32_t Randomizer_C_get_branching_seed(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->get_branching_seed();
 }
 
+
 bool Randomizer_C_gen_bool(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->gen_bool();
 }
+
 
 int Randomizer_C_gen_string(Randomizer_C* rndc, char* output_str, size_t out_str_length, const char* dictionary){
     if(output_str == NULL){
@@ -133,18 +152,46 @@ int Randomizer_C_gen_string(Randomizer_C* rndc, char* output_str, size_t out_str
     return 0;
 }
 
+
 double Randomizer_C_gen_double_not_nan(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->gen_double_not_nan();
 }
+
 
 double Randomizer_C_gen_double_not_nan_range(Randomizer_C* rndc, double lower, double upper){
     return reinterpret_cast<Randomizer*>(rndc)->gen_double_not_nan_range(lower, upper);
 }
 
+
 uint64_t Randomizer_C_gen_integral(Randomizer_C* rndc){
     return reinterpret_cast<Randomizer*>(rndc)->gen_integral<uint64_t>();
 }
 
+
 uint64_t Randomizer_C_gen_integral_range(Randomizer_C* rndc, uint64_t lower, uint64_t upper){
     return reinterpret_cast<Randomizer*>(rndc)->gen_integral_range<uint64_t>(lower, upper);
+}
+
+
+int Randomizer_C_shuffle(Randomizer_C* rndc, void* array, size_t n_elements, size_t elem_size){
+    size_t rand_idx;
+    // Check for zeros or NULL
+    if(array == NULL || n_elements == 0 || elem_size == 0){
+        return -1;
+    }
+    // Nothing to shuffle
+    if(n_elements == 1){
+        return 0;
+    }
+    uint8_t* buff;
+    buff = (uint8_t*)calloc(1, elem_size);
+    // Variation of the Fisher-Yates shuffling algorithm
+    for(size_t i = 0; i < n_elements - 1; i++){
+        rand_idx = Randomizer_C_gen_integral_range(rndc, i, (n_elements - 1));
+        memcpy((uint8_t*)buff, (uint8_t*)array + (i * elem_size), elem_size);
+        memcpy((uint8_t*)array + (i * elem_size), (uint8_t*)array + (rand_idx * elem_size), elem_size);
+        memcpy((uint8_t*)array + (rand_idx * elem_size), (uint8_t*)buff, elem_size);
+    }
+    free(buff);
+    return 0;
 }
